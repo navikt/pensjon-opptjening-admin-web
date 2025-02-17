14102b8d53c2296e2e29bbbfffd6995fdc6dd9a3
Jan Kroken <jan.kroken@nav.no>
2025-02-17 13:12:43 +0100

    under utvikling

--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/spring/PoppClientConfig.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/spring/PoppClientConfig.kt
@@ -1,18 +1,14 @@
 package no.nav.pensjon.opptjening.adminweb.external.spring
 
 import no.nav.pensjon.opptjening.adminweb.external.PoppClient
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import pensjon.opptjening.azure.ad.client.TokenProvider
 
 @Configuration
 internal class PoppClientConfig(
     @Value("\${POPP_URL}") private val baseUrl: String,
-//    @Qualifier("poppTokenProvider") private val tokenProvider: TokenProvider,
 ) {
     private val poppClient = PoppClient(
         baseUrl = baseUrl,
-//        tokenProvider = tokenProvider,
     )
 }
