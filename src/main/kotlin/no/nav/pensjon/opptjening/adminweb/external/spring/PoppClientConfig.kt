9dfcea7d3ef0c1bf94c5c8baa0da46e0432b38d0
Jan Kroken <jan.kroken@nav.no>
2025-02-18 17:04:28 +0100

    under utvikling

--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/spring/PoppClientConfig.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/spring/PoppClientConfig.kt
@@ -1,19 +1,21 @@
 package no.nav.pensjon.opptjening.adminweb.external.spring
 
 import no.nav.pensjon.opptjening.adminweb.external.FilAdapterKlient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import pensjon.opptjening.azure.ad.client.TokenProvider
 
 @Configuration
-internal class PoppClientConfig(
+class PoppClientConfig(
+    @Value("\${FILADAPTER_URL}") private val baseUrl: String,
     private val poppTokenProvider: TokenProvider,
 ) {
 
     @Bean
-    open fun filAdapterKlient(): FilAdapterKlient {
+    fun filAdapterKlient(): FilAdapterKlient {
         return FilAdapterKlient(
-            baseUrl = "http://banan",
+            baseUrl = "baseUrl",
             nextToken = { poppTokenProvider.getToken() }
         )
     }
