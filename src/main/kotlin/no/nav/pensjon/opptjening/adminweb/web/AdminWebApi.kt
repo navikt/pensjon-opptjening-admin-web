144d0838175a7f9729d3a3bdb77717490421b2e0
Jan Kroken <jan.kroken@nav.no>
2025-02-18 10:12:19 +0100

    under utvikling

--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminWebApi.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminWebApi.kt
@@ -3,8 +3,6 @@ package no.nav.pensjon.opptjening.adminweb.web
 import no.nav.security.token.support.core.api.Protected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE
-import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,9 +15,16 @@ class AdminWebApi() {
         private val secureLog: Logger = LoggerFactory.getLogger("secure")
     }
 
-    @GetMapping("/hello", consumes = [APPLICATION_FORM_URLENCODED_VALUE], produces = [TEXT_PLAIN_VALUE])
+    @GetMapping("/hello")
     fun sayHello(
     ): ResponseEntity<String> {
         return ResponseEntity.ok("Hello world")
     }
+
+    @GetMapping("/ping")
+    fun ping(): ResponseEntity<String> {
+        log.info("Sa hei")
+        return ResponseEntity.ok("pong")
+    }
+
 }
