d6db9db24fd283212ef97686c988651667e3aaf2
Jan Kroken <jan.kroken@nav.no>
2025-02-19 12:19:11 +0100

    under utvikling

--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminWebApi.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminWebApi.kt
@@ -1,9 +1,8 @@
 package no.nav.pensjon.opptjening.adminweb.web
 
 import no.nav.pensjon.opptjening.adminweb.external.FilAdapterKlient
+import no.nav.pensjon.opptjening.adminweb.log.NAVLog
 import no.nav.security.token.support.core.api.Protected
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,18 +13,23 @@ class AdminWebApi(
     private val filAdapterKlient: FilAdapterKlient,
 ) {
     companion object {
-        private val log: Logger = LoggerFactory.getLogger(AdminWebApi::class.java)
-        private val secureLog: Logger = LoggerFactory.getLogger("secure")
+        private val log = NAVLog(AdminWebApi::class)
     }
 
     @GetMapping("/list")
     fun listFiler(): ResponseEntity<String> {
-        return ResponseEntity.ok(filAdapterKlient.listFiler())
+        return try {
+            ResponseEntity.ok(filAdapterKlient.listFiler())
+        } catch (e: Throwable) {
+            log.open.warn("Feil ved listing av filer: ${e.message}")
+            log.secure.warn("Feil ved listing av filer: ${e.message}", e)
+            ResponseEntity.internalServerError().body("Intern feil")
+        }
     }
 
     @GetMapping("/ping")
     fun ping(): ResponseEntity<String> {
-        log.info("Sa hei")
+        log.open.info("Sa hei")
         return ResponseEntity.ok("pong")
     }
 
