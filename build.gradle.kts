3f9a2faf55d56e16a505a44565941fcfe06c1e05
Jan Kroken <jan.kroken@nav.no>
2025-06-10 12:49:17 +0200

    oppdaterte avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,32 +4,32 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val azureAdClient = "0.0.7"
-val jacksonVersion = "2.17.1"
+val jacksonVersion = "2.19.0"
 val logbackEncoderVersion = "8.0"
 val springCloudContractVersion = "4.0.4"
-val mockkVersion = "1.13.16"
+val mockkVersion = "1.14.2"
 val assertJVersion = "3.27.3"
-val jsonUnitVersion = "4.1.0"
-val wiremockVersion = "3.11.0"
+val jsonUnitVersion = "4.1.1"
+val wiremockVersion = "3.13.0"
 val mockitoVersion = "5.4.0"
-val navTokenSupportVersion = "5.0.16"
+val navTokenSupportVersion = "5.0.29"
 val hibernateValidatorVersion = "8.0.1.Final"
 val junit5Version = "5.11.3"
 val okHttpVersion = "1111"
 
 
-val snakeYamlVersion = "2.3"
+val snakeYamlVersion = "2.4"
 val snappyJavaVersion = "1.1.10.7"
 // val httpClient5Version = "5.3.1" // TODO: 5.4 feiler med NoClassDefFoundError
-val httpClient5Version = "5.4.2" // TODO: 5.4 feiler med NoClassDefFoundError
+val httpClient5Version = "5.5" // TODO: 5.4 feiler med NoClassDefFoundError
 val httpClientVersion = "4.5.14" // deprecated, men brukes av
 
 plugins {
-    val kotlinVersion = "2.0.20"
+    val kotlinVersion = "2.1.21"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "3.4.2"
+    id("org.springframework.boot") version "3.5.0"
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
@@ -125,6 +125,6 @@ tasks.withType<DependencyUpdatesTask>().configureEach {
 }
 
 fun isNonStableVersion(version: String): Boolean {
-    return listOf("BETA","RC","-M",".CR").any { version.uppercase().contains(it) }
+    return listOf("BETA","RC","-M",".CR","ALPHA").any { version.uppercase().contains(it) }
 }
 
