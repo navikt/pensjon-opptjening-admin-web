59cf60975a3575382c5991b30a5ed8cd806c25c1
Jan Kroken <jan.kroken@nav.no>
2025-08-21 12:44:02 +0200

    oppdaterte avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,31 +3,29 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val azureAdClient = "0.0.7"
-val jacksonVersion = "2.19.0"
-val logbackEncoderVersion = "8.0"
-val springCloudContractVersion = "4.0.4"
-val mockkVersion = "1.14.2"
-val assertJVersion = "3.27.3"
+val jacksonVersion = "2.19.2"
+val logbackEncoderVersion = "7.4"
+val mockkVersion = "1.14.5"
+val assertJVersion = "3.27.4"
 val jsonUnitVersion = "4.1.1"
-val wiremockVersion = "3.13.0"
-val mockitoVersion = "5.4.0"
-val navTokenSupportVersion = "5.0.29"
+val wiremockVersion = "3.13.1"
+val mockitoVersion = "6.0.0"
+val navTokenSupportVersion = "5.0.34"
 val hibernateValidatorVersion = "8.0.1.Final"
-val junit5Version = "5.11.3"
+val junit5Version = "5.11.4"
+val okHttpVersion = "5.1.0"
 
 
 val snakeYamlVersion = "2.4"
 val snappyJavaVersion = "1.1.10.7"
-// val httpClient5Version = "5.3.1" // TODO: 5.4 feiler med NoClassDefFoundError
 val httpClient5Version = "5.5" // TODO: 5.4 feiler med NoClassDefFoundError
-val httpClientVersion = "4.5.14" // deprecated, men brukes av
 
 plugins {
-    val kotlinVersion = "2.1.21"
+    val kotlinVersion = "2.2.10"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.5.4"
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
@@ -56,7 +54,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework:spring-aspects")
     implementation("no.nav.security:token-validation-spring:$navTokenSupportVersion")
@@ -87,6 +84,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
 
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:$jsonUnitVersion")
 
 }
