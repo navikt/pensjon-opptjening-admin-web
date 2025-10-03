cbc2908b3b1767867e16454a233cdb267f2a89f9
Jan Kroken <jan.kroken@nav.no>
2025-10-03 12:57:54 +0200

    grensesnitt for pgi (pågår)

--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/PoppKlient.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/external/PoppKlient.kt
@@ -3,10 +3,15 @@ package no.nav.pensjon.opptjening.adminweb.external
 import no.nav.pensjon.opptjening.adminweb.log.NAVLog
 import no.nav.popp.web.api.endpoint.pgi.model.PgiInnlesingHentRequest
 import no.nav.popp.web.api.endpoint.pgi.model.PgiInnlesingHentResponse
+import no.nav.popp.web.api.endpoint.pgi.model.PgiInnlesingListFeiledeResponse
 import no.nav.popp.web.api.endpoint.pgi.model.PgiInnlesingStatusResponse
 import org.apache.hc.core5.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
 
@@ -47,13 +52,47 @@ class PoppKlient(
             return restClient
                 .get()
                 .uri("/pgi/status")
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .body<PgiInnlesingStatusResponse>()!!
         } catch (t: Throwable) {
             log.secure.warn("kunne ikke hente status for PGI-innelsing", t)
             throw PoppKlientException("Kunne ikke hente status for PGI-innlesing", t)
         }
+    }
+
+    fun hentPgiForPersonOgÅr(request: PgiInnlesingHentRequest): PgiInnlesingHentResponse {
+        return restClient
+            .post()
+            .uri("/pgi/hent")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .retrieve()
+            .body<PgiInnlesingHentResponse>()!!
+
+    }
+
+    fun hentFeiledePgi(): PgiInnlesingListFeiledeResponse {
+        try {
+            return restClient
+                .get()
+                .uri("/pgi/status")
+                .retrieve()
+                .body<PgiInnlesingListFeiledeResponse>()!!
+        } catch (t: Throwable) {
+            log.secure.warn("kunne ikke hente status for PGI-innelsing", t)
+            throw PoppKlientException("Kunne ikke hente status for PGI-innlesing", t)
+        }
+    }
+
+    @PostMapping(
+        path = ["/feilet/rekjor"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    // TODO: Tilby senere
+    fun rekjorFeilede() {
+        // /feilet/rekjor
+        throw NotImplementedError("Ikke implementert enda")
     }
 
     class PoppKlientException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
--- src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminResource.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/adminweb/web/AdminResource.kt
@@ -4,6 +4,7 @@ import no.nav.pensjon.opptjening.adminweb.external.FilAdapterKlient
 import no.nav.pensjon.opptjening.adminweb.external.PoppKlient
 import no.nav.pensjon.opptjening.adminweb.log.NAVLog
 import no.nav.pensjon.opptjening.adminweb.utils.JsonUtils.toJson
+import no.nav.popp.web.api.endpoint.pgi.model.PgiInnlesingHentRequest
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -91,9 +92,47 @@ class AdminResource(
         }
     }
 
+    @PostMapping("/pgi/synkroniser-person")
+    fun pgiStatus(
+        @RequestParam("fnr") fnr: String,
+        @RequestParam("ar") ar: Int,
+    ): ResponseEntity<String> {
+        return try {
+            if (!gyldigFnrInput(fnr)) {
+                log.open.warn("synkroniser-person: ugyldige tegn i fnr")
+                ResponseEntity.badRequest().body("Ugyldig fnr")
+            } else {
+                val response = poppKlient.hentPgiForPersonOgÅr(
+                    PgiInnlesingHentRequest(fnr, ar)
+                ).toJson()
+                ResponseEntity.ok(response.toJson())
+            }
+        } catch (t: Throwable) {
+            log.open.warn("Kunne ikke synkronisere person og år")
+            log.secure.warn("Kunne ikke synkronisere person og år", t)
+            ResponseEntity.internalServerError().body("Intern feil")
+        }
+    }
+
+    @GetMapping("/pgi/list-feilede")
+    fun pgiListFeilede(): ResponseEntity<String> {
+        return try {
+            ResponseEntity.ok(poppKlient.hentFeiledePgi().toJson())
+        } catch (t: Throwable) {
+            log.open.warn("Kunne ikke liste feilede")
+            log.secure.warn("Kunne ikke liste feilede", t)
+            ResponseEntity.internalServerError().body("Intern feil")
+        }
+    }
+
+
     @GetMapping("/ping")
     fun ping(): ResponseEntity<String> {
         log.open.info("Sa hei")
         return ResponseEntity.ok("pong")
+    }
+
+    fun gyldigFnrInput(fnr: String): Boolean {
+        return fnr.matches("^[0-9]*$".toRegex())
     }
 }
